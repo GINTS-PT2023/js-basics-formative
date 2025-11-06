f92e35777571730ee757686f9e9daa16f29d8045
Emils <emils@gmail.com>
2025-11-06 10:42:41 +0200

    completed 4.uzdevums

---
+++ people-array.js
@@ -0,0 +1,16 @@
+let people = [
+    {
+        name: "Peteris",
+        age: 76,
+        isStudent: false
+    },
+     {
+        name: "Emils",
+        age: 22,
+        isStudent: true
+    }
+];
+
+for (let i = 0; i < people.length; i++){
+    console.log("Vārds: " + people[i].name);
+}
